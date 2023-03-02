31247e5d1ddbc4339bf19e19ec5f4c5a48c84238
Antoine Garcia <a.garcia.walecha@gmail.com>
2023-03-02 11:08:08 +0100

    add login page with supabase login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,12 @@
+'use client'
+
+import { supabase } from "../../clients/supabase"
+
 export default function Login() {
   return (
     <>
       <h1>this is the login page</h1>
-      <button className="btn">Login</button>
+      <button className="btn" onClick={() => supabase.auth.signInWithOAuth({ provider: 'github' })}>Login with github</button>
     </>
   )
 }
