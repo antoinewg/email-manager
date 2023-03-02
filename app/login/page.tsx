6713af6fb1f1b92bf7572605c37aae99b2b51d14
Antoine Garcia <a.garcia.walecha@gmail.com>
2023-03-02 11:15:38 +0100

    add encoding

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,10 +3,19 @@
 import { supabase } from "../../clients/supabase"
 
 export default function Login() {
+  async function handleSignIn() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'github'
+    })
+
+    console.log(data, error)
+  }
+
+
   return (
     <>
       <h1>this is the login page</h1>
-      <button className="btn" onClick={() => supabase.auth.signInWithOAuth({ provider: 'github' })}>Login with github</button>
+      <button className="btn" onClick={handleSignIn}>Login with github</button>
     </>
   )
 }
