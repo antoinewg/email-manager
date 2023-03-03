8af7d3d57ea6546753a33922d2e380fa3d3fb63c
Antoine Garcia <a.garcia.walecha@gmail.com>
2023-03-03 11:15:58 +0100

    add seed

---
+++ supabase/seed.sql
@@ -0,0 +1,6 @@
+insert into next_auth.users
+  (name, email)
+values
+  ('Erlich Bachman', 'erlich@bachman.com'),
+  ('Richard Hendricks', 'richard@hendricks.com'),
+  ('Monica Hall', 'monica@hall.com');
