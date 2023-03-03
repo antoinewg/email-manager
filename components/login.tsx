8203a1010274d530cea5a792e98a74d269d4df32
Antoine Garcia <a.garcia.walecha@gmail.com>
2023-03-03 13:07:40 +0100

    login with next-auth

--- components/login.tsx
+++ components/login.tsx
@@ -1,49 +1,13 @@
 'use client';
 
-import { useSupabase } from './supabase-provider';
+import { signIn } from "next-auth/react"
 
 // Supabase auth needs to be triggered client-side
 export default function Login() {
-  const { supabase, session } = useSupabase();
-
-  const handleEmailLogin = async () => {
-    const { error } = await supabase.auth.signInWithPassword({
-      email: 'jon@supabase.com',
-      password: 'password'
-    });
-
-    if (error) {
-      console.log({ error });
-    }
-  };
-
-  const handleGitHubLogin = async () => {
-    const { error } = await supabase.auth.signInWithOAuth({
-      provider: 'github'
-    });
-
-    if (error) {
-      console.log({ error });
-    }
-  };
-
-  const handleLogout = async () => {
-    const { error } = await supabase.auth.signOut();
-
-    if (error) {
-      console.log({ error });
-    }
-  };
-
-  // this `session` is from the root loader - server-side
-  // therefore, it can safely be used to conditionally render
-  // SSR pages without issues with hydration
-  return session ? (
-    <button onClick={handleLogout}>Logout</button>
-  ) : (
+  return (
     <>
-      <button onClick={handleEmailLogin}>Email Login</button>
-      <button onClick={handleGitHubLogin}>GitHub Login</button>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
     </>
-  );
+  )
 }
