f6a7c0802bd1a7058892c464c2db5843182f5b8d
Antoine Garcia <a.garcia.walecha@gmail.com>
2023-03-03 12:11:13 +0100

    add github provider for next auth

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -2,17 +2,22 @@
 import NextAuth from "next-auth"
 import { SupabaseAdapter } from "@next-auth/supabase-adapter"
 import jwt from "jsonwebtoken"
+import GithubProvider from "next-auth/providers/github"
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export default NextAuth({
-  // https://next-auth.js.org/configuration/providers
-  providers: [],
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? '',
+      clientSecret: process.env.GITHUB_SECRET ?? '',
+    }),
+  ],
   adapter: SupabaseAdapter({
     url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
   }),
-    callbacks: {
+  callbacks: {
     async session({ session, user }) {
       const signingSecret = process.env.SUPABASE_JWT_SECRET
       let supabaseAccessToken: string | undefined = undefined
